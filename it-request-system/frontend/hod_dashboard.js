769a750fa67e55c3fc3bb7a61ce1e0928a3ed876
Naveen Rathi <102209027+Naveenrathi9@users.noreply.github.com>
2025-05-06 09:07:05 +0530

    Update hod_dashboard.js

--- it-request-system/frontend/hod_dashboard.js
+++ it-request-system/frontend/hod_dashboard.js
@@ -1,5 +1,5 @@
-const BASE_URL = "http://localhost:5000";
-//const BASE_URL = "https://jpl-z0s7.onrender.com";
+//const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://jpl-backend.onrender.com";
 
 // Check if user is logged in
 function checkAuth() {
